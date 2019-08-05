011141382d8ffdf2b8640c31af1ced08e40462bc
Juan Sebastian Osorio Garcia <seposorio@hotmail.com>
2019-08-05 13:46:48 -0500

    Primeros Algoritmos

---
+++ Talleres/PrimerCorte/Implementación Algoritmos/posicionFalsaAlgoritmo.py
@@ -0,0 +1,49 @@
+
+import numpy as np
+import math
+
+#Main
+print("Algortimo Posicion Falsa")
+print("---------------------------------")
+
+# Definición de la Función f(x)
+def f(x):
+    y = math.e**x - math.pi * x
+    return y
+#Definición formula Posicion falsa
+def formuPosicionFalsa(x0,x1,iterr):
+    x = x0 - ((x1-x0)/(f(x1)-f(x0)))*f(x0)
+    iterr+=1
+    #Impresion iteraciones
+    print("#", iterr, " X= ", x1, "                       F(x)= ", f(x1))
+    return x,iterr
+
+
+def PosicionFalsa(x0,x1,numIter,tol):
+    iterr=0
+    x,iterr = formuPosicionFalsa(x0,x1,iterr)
+    while iterr<numIter:
+        if f(x)*f(x0) < 0:
+            x0 = x
+        else:
+            x1 = x
+        x3,iterr = formuPosicionFalsa(x0,x1,iterr)
+        if abs(x3-x) < tol:
+
+            # Resultado Raices
+            print(" ")
+            print("Existe raiz en: ", x3)
+            print("Número de Iteraciones", iterr)
+            print(" ")
+
+            break
+        x=x3
+    if iterr == numIter:
+        print('No se encontro el resultado, pruebe con mas iteraciones')
+
+#llamada por intevalos
+PosicionFalsa(0,1,100,10e-8)
+PosicionFalsa(1,2,100,10e-8)
+
+
+
